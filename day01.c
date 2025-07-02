10ed744e1f6c33b5508162c4344d257eb726a921
ssung <sungsharon@gmail.com>
2025-07-01 20:05:14 -0400

    feat: day 1... done

--- day01.c
+++ day01.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 char turn(char *position, char *direction) {
     if (*direction == 'R') {
         if (*position == 'N') {
@@ -71,8 +72,66 @@ int part1() {
     return blocks;
 }
 
+int part2() {
+    FILE *file = fopen("data/day1", "r");
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* buffer = (char*)malloc(file_size+1);
+    size_t bytes_read = fread(buffer, 1, file_size, file);
+    buffer[file_size] = '\0';
+
+    int coords[1000][2];
+    int current_pos = 0;
+    int x = 0, y = 0;
+    char direction = 'N';
+
+    char* token = strtok(buffer, " ");
+    while (token != NULL) {
+        char lr[2];
+        memcpy(lr, &token[0], 1);
+        lr[1] = '\0';
+
+        int value = atoi(token +1);
+
+        if (*lr == 'R' || *lr == 'L') {
+            direction = turn(&direction, lr);
+        }
+
+        for (int i = 0; i < value; i++) {
+            if (direction == 'N') {
+                y += 1;
+            } else if (direction == 'E') {
+                x += 1;
+            } else if (direction == 'S') {
+                y -= 1;
+            } else if (direction == 'W') {
+                x -= 1;
+            }
+
+            // create the coordinate
+            coords[current_pos][0] = x;
+            coords[current_pos][1] = y;
+
+            for (int j = 0; j < current_pos; j++) {
+                if (coords[j][0] == x && coords[j][1] == y) {
+                    int blocks = abs(x) + abs(y);
+                    fclose(file);
+                    return blocks;
+                }
+            }
+            current_pos++;
+        }
+
+        token = strtok(NULL, " ");
+    }
+}
+
 int main() {
     printf("Part 1: %d\n", part1());
+    printf("Part 2: %d\n", part2());
 
     return 0;
 }
